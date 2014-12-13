680c6edd954b1bc7f61f851e62c8ddbe6ddc8ad9
Damir Mirgadiev <yoozheg@gmail.com>
2014-12-13 12:34:47 +0500

    begin

---
+++ shop/script.js
@@ -0,0 +1,47 @@
+(function(window, document, $, undefined){
+ "use strict";
+ 
+ var counter = { // Дата для счётчика
+  date : new Date(2014,9,8), // Год, Месяц-1, День (сейчас стоит 8 ноября 2014 года)
+  delay: 5 // скрипт перезапускается каждые 5 дней, если установленная дата была ранее
+ };
+ 
+ window.slider = {before:'content/slides', after:'.jpg', start:1, count:3};
+ 
+ $.ready(function(){
+  var forms   = $(['form']),
+    countdown = $(['.countdown']),
+    animated  = $(['.animated']),
+    splash    = $(['.splash']),
+    numbers   = $('.numbers'),
+    gallery   = $('.gallery'),
+    slideShow = $('.slider');
+
+  if(gallery)   $.gallery(gallery);
+  if(countdown) $.countdown(counter.date, counter.delay);
+  if(slideShow) $.slider(slideShow, window[slideShow.data('content')]);
+  if(splash)    splash.on({click: function(event){ $.splash.open({onclose: hidePopup}); showPopup() }});
+  if(forms)     forms .on({submit: sendForm});
+  
+  window.onscroll = function(){
+   var top = window.pageYOffset || document.documentElement.scrollTop, height = $.client.height(), pos = top+height/1.7;
+   if(numbers && pos > numbers.coords().top) numbers.find(['p span']).each(function(){increment(this)});
+   if(animated) animated.each(function(){ (pos > this.coords().top) ? this.addClass('scroll') : this.removeClass('scroll') });
+   
+   function increment(e){
+    var need = parseInt(e.data('content')), current = parseInt(e.html());
+    if(current < need) e.html(++current);
+    if(current < need) setTimeout(function(){increment(e)}, 2500/need);
+   }
+  }
+  $(['#houseplan button']).on({click:function(e){
+   this.onceClass('selected');
+   this.parent().find(['img']).hide();
+   this.parent().find('img[src*="'+this.data('img')+'"]').show();
+  }});
+ });
+ 
+ function showPopup(){ $('#hided').show() }
+ function hidePopup(){ $('#hided').hide() }
+ function sendForm(event){ return $.form(this) }
+})(window, document, $);
